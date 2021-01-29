6ab05f80f5d2c9e8719b873323d030a27153200a
vishal929 <48373882+vishal929@users.noreply.github.com>
2021-01-29 14:58:46 -0500

    Worked a little on prelim functions needed and GUI stuff (GUI stuff still not done)

---
+++ Preliminaries/mazeGenerator.py
@@ -0,0 +1,106 @@
+# mazeGenerator.py includes functions directly related to the maze
+# need random number generator for obstacleDensity
+from random import random
+from random import randint
+
+# grid is just list of ints, if 0 then the grid is free, if 1 then its blocked, if -1 then it is on fire
+#if the entry is 2, then the agent is present
+
+
+# just populates a maze based on dimension and obstacleDensity
+# if dim<2 then exception raised
+# obstacleDensity is a probability from 0 to 1, if anything else inputted, then exception is raised
+def generateMaze(dim, obstacleDensity):
+    if dim<2:
+        raise ValueError("Dimension cannot be less than 2!")
+    if obstacleDensity<0 or obstacleDensity>1:
+        raise ValueError("Blocking factor must be a probability from 0 to 1!")
+    # so far we passed checks, so generating list
+    maze=[]
+    for i in range(dim):
+        toAdd=[]
+        for j in range(dim):
+            if i==0 and j==0:
+                # top left corner is the start
+                toAdd.append(0)
+            elif i==dim-1 and j==dim-1:
+                # bottom right corner is the end
+                toAdd.append(0)
+            else:
+                # generating random number for obstacleDensity probability
+                randomNumber = random()
+                if randomNumber<obstacleDensity:
+                    # then this is an obstacle entry
+                    toAdd.append(1)
+                else:
+                    # then this is a free entry
+                    toAdd.append(0)
+        # appending the row list to the maze
+        maze.append(toAdd)
+    return maze
+
+# method below for initializing fire in the maze
+def initializeFire(maze, dim):
+    row = randint(0,dim-1)
+    col = randint(0,dim-1)
+    # randomly chosen position above will be the starting point for the fire
+    maze[row][col]=-1
+
+
+
+# method below for performing fire generation given flammability rate
+def lightMaze(maze,dim,flammabilityRate):
+   if flammabilityRate<0 or flammabilityRate>1:
+       raise ValueError("Flammability rate must be between 0 and 1 (inclusive)!")
+   # iterating and adjusting fire
+   for i in range(dim):
+       for j in range(dim):
+          if maze[i][j]!=1 and maze[i][j]!=-1:
+            # then I need to check neighbors
+            neighbors = []
+            numFire=0
+            neighbors.append((i+1, j))
+            neighbors.append((i+1, j+1))
+            neighbors.append((i + 1, j-1))
+            neighbors.append((i-1, j))
+            neighbors.append((i - 1, j+1))
+            neighbors.append((i - 1, j-1))
+            neighbors.append((i, j+1))
+            neighbors.append((i, j-1))
+            for neighbor in neighbors:
+                if neighbor[0]<dim and neighbor[0]>=0 and neighbor[1]<dim and neighbor[1]>=0:
+                    # then this is valid neighbor to check
+                    if maze[neighbor[0]][neighbor[1]]==-1:
+                        numFire += 1
+            probFire = 1-((1-flammabilityRate)**numFire)
+            if random()<probFire:
+                # then now this cell is on fire
+                maze[i][j] = -1
+
+# prints the maze to stdout
+# O=open/free, F=fire, B=blocked, A=agent
+def printMaze(maze,dim):
+
+    for i in range(dim):
+        for j in range(dim):
+            print("---")
+        print("\n")
+        for j in range(dim):
+            print("|")
+            if maze[i][j]==0:
+                # o for open
+                print("O")
+            elif maze[i][j]==-1:
+                # f for fire
+                print("F")
+            elif maze[i][j]==2:
+                #A for agent
+                print("A")
+            else:
+                # b for blocked
+                print("B")
+            print("|")
+        print("\n")
+    for i in range(dim):
+        print("---")
+    print("\n")
--- README.md
+++ README.md
@@ -3,10 +3,14 @@ Maze Project for AI class at Rutgers
 Worked on by Vishal Patel and Nilay Naik
 
 #Preliminaries
+
 ##Maze Generator
 mazeGenerator will provide the logic to generate a grid
 based on dimension input and an obstacle density
-(probability of a square being an obstacle or not)
+(probability of a square being an obstacle or not).
+
+In addition, mazeGenerator provides logic to populate the 
+maze with fire, given a flammability rate.
 
 ##DFS
 
@@ -23,4 +27,7 @@ based on dimension input and an obstacle density
 #Our Strategy
 
 #Maze Visualization
-Provides Logic for visualizing the grid
+Provides Logic for visualizing the grid.
+This part is optional for running the program
+and the user decides if they want to see the 
+visualization or not. 
